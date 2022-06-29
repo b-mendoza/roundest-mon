29eabb0ab56c02d23fda7f0cae1d01d43b603653
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-06-28 21:44:39 -0600

    feat: add initial UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
 export default function IndexPage() {
   return (
-    <main>
-      <h1>Hello World</h1>
-    </main>
+    <div className="h-screen flex flex-col justify-center">
+      <h1 className="text-4xl font-extralight mb-8">
+        Which Pokémon is Rounder?
+      </h1>
+
+      <main className="border rounded p-8">
+        {/* First Pokémon */}
+        <div />
+
+        <p>Vs.</p>
+
+        {/* Second Pokémon */}
+        <div />
+      </main>
+    </div>
   );
 }
