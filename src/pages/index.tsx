1cf723c40e187dd5ef0400585c9ea8ad4e5123e4
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-06-28 23:42:48 -0600

    feat: add initial setup to `IndexPage` component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import { trpc } from '@/utils/trpc';
 
 export default function IndexPage() {
-  const { data, isLoading } = trpc.useQuery(['hello', { text: 'Bryan' }]);
+  const { data: firstPokemonData, isLoading: isFirstPokemonLoading } =
+    trpc.useQuery(['get-pokemon-by-id', { id: 2 }]);
+
+  const { data: secondPokemonData, isLoading: isSecondPokemonLoading } =
+    trpc.useQuery(['get-pokemon-by-id', { id: 1 }]);
+
+  const isLoading = isFirstPokemonLoading || isSecondPokemonLoading;
 
   if (isLoading) return <h1>Loading . . .</h1>;
-
-  if (data) return <h1>{data.greeting}</h1>;
 
   return (
     <div className="h-screen flex flex-col justify-center">
@@ -14,13 +18,11 @@ export default function IndexPage() {
       </h1>
 
       <main className="border rounded p-8">
-        {/* First Pokémon */}
-        <div />
+        <div>{firstPokemonData?.name}</div>
 
         <p>Vs.</p>
 
-        {/* Second Pokémon */}
-        <div />
+        <div>{secondPokemonData?.name}</div>
       </main>
     </div>
   );
