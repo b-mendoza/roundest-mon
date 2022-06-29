882bb48cc135a803e75d8a75b81ad509a629bfe6
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-06-29 00:21:10 -0600

    chore: add `swcMinify` as `true`

--- next.config.js
+++ next.config.js
@@ -3,10 +3,11 @@ const { withPlaiceholder } = require('@plaiceholder/next');
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     domains: ['raw.githubusercontent.com'],
   },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = withPlaiceholder(nextConfig);
