2875e7bbd1f0c163f556de11212066acd235003e
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-06-29 01:07:22 -0600

    feat: add `lang` attribute to document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function NextDocument() {
+  return (
+    <Html lang="en-US">
+      <Head />
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
