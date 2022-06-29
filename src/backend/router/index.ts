afb8a7b8d9de869eca4c7623b8b75a8b1b3e8683
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-06-28 23:41:30 -0600

    feat: add `get-pokemon-by-id` route

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,15 +1,16 @@
 import * as trpc from '@trpc/server';
+import { PokemonClient } from 'pokenode-ts';
 import { z } from 'zod';
 
-export const appRouter = trpc.router().query('hello', {
-  input: z
-    .object({
-      text: z.string().nullish(),
-    })
-    .nullish(),
-  resolve({ input }) {
-    return {
-      greeting: `hello ${input?.text ?? 'world'}`,
-    };
+const pokemonAPI = new PokemonClient();
+
+export const appRouter = trpc.router().query('get-pokemon-by-id', {
+  input: z.object({
+    id: z.number(),
+  }),
+  async resolve({ input }) {
+    const pokemon = await pokemonAPI.getPokemonById(input.id);
+
+    return pokemon;
   },
 });
